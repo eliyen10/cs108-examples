3eba390b6f91306cdfef0515911a52f2940159b6
Eli Yen <eliyen10@bu.edu>
2021-11-04 20:33:14 -0400

    second step of the way

---
+++ mini_fb/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import ShowAllProfilesView
+
+urlpatterns = [
+    path('', ShowAllProfilesView.as_view(), name="home_page"),
+]
+
+
+
+    # ShowAllProfilesView
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Profile
 # Create your views here.
+
+class ShowAllProfilesView(ListView):
+    '''Shows how the layout of mini fb will be.'''
+    model = Profile
+    template_name = "mini_fb/show_all_profiles.html"
+    context_object_name = "mini_fb"
